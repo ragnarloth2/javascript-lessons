cf9f8636527913df379d08912e4b5bdf392ee33c
ragnarloth2 <blacktopmine@hotmail.com>
2019-02-08 18:28:05 -0200

    Mais opções de destructuring

--- destructuring2.js
+++ destructuring2.js
@@ -2,3 +2,21 @@
 let verificadorNota = notaAluno => notaAluno >= 5 ? 'Aprovado!' : 'Reprovado!'
 
 console.log(verificadorNota(5))
+
+// destructuring com objetos
+
+let escola = {
+    numero_estudantes : 30,
+    disciplinas: {
+        portugues: true,
+        matematica: true,
+        ingles: false
+    }
+}
+
+let {numero_estudantes, disciplinas: {portugues,matematica,ingles}} = escola
+
+console.log(`Numero estudantes: ${numero_estudantes},
+Disciplinas: Portugues: ${portugues},
+Matemática: ${matematica},
+Inglês: ${ingles}`)
