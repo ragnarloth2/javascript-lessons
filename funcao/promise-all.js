eb45eed8641a2c6adc2a694e21563c98d8453133
Renan Cesar <blacktopmine@hotmail.com>
2019-07-30 17:29:04 -0300

    promise content

--- funcao/promise-all.js
+++ funcao/promise-all.js
@@ -1,1 +1,42 @@
-file:///home/renan/Desktop/npm/index.js
+function getUser() {
+    return new Promise((resolve, reject) => {
+        setTimeout(_ => {
+            return resolve({
+                id: 1,
+                name: 'Renan Cesar'
+            })
+        },1000)
+    })
+}
+
+function getPhone(userId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(_ => {
+            return resolve({
+                id:1,
+                number: '21312312'
+            })
+        },1000)
+    })
+}
+
+main()
+async function main() {
+    try {
+        console.time('medida-promise')
+        const user = await getUser()
+        /* 
+            Nesta parte e interessante notar que o Promise.all faz o processamento de todas 3 funçoes de forma
+            assincrona, demorando assim, 2s totais para completar 3 timers de 1s.
+       */
+        const promiseObj = await Promise.all([getPhone(user.id),getPhone(user.id),getPhone(user.id)])
+        const phone = promiseObj[0]
+        const phone2 = promiseObj[1]
+        const phone3 = promiseObj[2]
+        console.log(user.name)
+        console.log(phone.number)
+        console.timeEnd('medida-promise')
+    } catch(error) {
+
+    }
+}
